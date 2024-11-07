8de735672dd682d1f3b419b48d5b6d361a30161e
Xie Yuheng <xyheme@gmail.com>
2024-11-08 05:54:26 +0800

    `free_port_group_t`
    - A `port_t` is a free port
      if it is not linked to a node
      and it's `port_index` is -1.

--- src/lang/free_port_group.c
+++ src/lang/free_port_group.c
@@ -1,1 +1,10 @@
 #include "index.h"
+
+// A `port_t` is a free port
+// if it is not linked to a node
+// and it's `port_index` is -1.
+
+struct free_port_group_t {
+    const node_spec_t *node_spec;
+    port_t *ports;
+};
