00ffb8650846f98f682f1b67d2ca6b1df14c0d11
Xie Yuheng <xyheme@gmail.com>
2024-11-07 17:14:47 +0800

    up

--- src/lang/worker.c
+++ src/lang/worker.c
@@ -1,7 +1,11 @@
 #include "index.h"
 
+// A worker is like a little forth inner interpreter.
+// The `port_stack` is like the value_stack of forth,
+// but in this inet every value is port, let's be pure.
+
 struct worker_t {
     mod_t *mod;
-    port_stack_t *port_stack;
     list_t *active_edge_list;
+    port_stack_t *port_stack;
 };
