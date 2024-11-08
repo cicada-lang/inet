02bbd9d474e79535aff0a509929c999cb89dd26b
Xie Yuheng <xyheme@gmail.com>
2024-11-08 17:53:36 +0800

    up

--- src/libs/command_runner/deps.h
+++ src/libs/command_runner/deps.h
@@ -1,16 +1,3 @@
 #pragma once
 
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <string.h>
-#include <stdbool.h>
-#include <ctype.h>
-
-#include "../allocate/index.h"
-#include "../string/index.h"
 #include "../list/index.h"
-#include "../file/index.h"
-#include "../stack/index.h"
