a280f5cf878d7ce36ebf1ef647339922409824fb
Xie Yuheng <xyheme@gmail.com>
2024-11-13 09:51:01 +0800

    `parser_test`

--- TODO.md
+++ TODO.md
@@ -1,5 +1,3 @@
-`parser_test`
-
 `worker_load` -- take `src` and `text`, create `mod` and call `worker_execute_stmt`
 
 # later
--- src/lang/parser_test.c
+++ src/lang/parser_test.c
@@ -4,7 +4,27 @@ void
 parser_test(void) {
     printf("<parser_test>\n");
 
-    //
+    list_t *stmt_list = parse("\
+* (zero) -- value!                   \
+* (add1) prev -- value!              \
+* (add) target! addend -- result     \
+                                     \
+! (zero)-(add)                       \
+  (add)-addend result-(add)          \
+                                     \
+! (add1)-(add)                       \
+  (add)-addend (add1)-prev add       \
+  add1 result-(add)                  \
+                                     \
+= one zero add1                      \
+= two one one add                    \
+= three two one add                  \
+= four two two add                   \
+                                     \
+. two two add two two add            \
+");
+
+    assert(list_length(stmt_list) == 10);
 
     printf("</parser_test>\n");
 }
