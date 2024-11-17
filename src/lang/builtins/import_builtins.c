4f194baf855ea108c5130c48900df99d091b7f0a
Xie Yuheng <xyheme@gmail.com>
2024-11-17 09:13:51 +0800

    extract `define_builtin`

--- src/lang/builtins/import_builtins.c
+++ src/lang/builtins/import_builtins.c
@@ -1,8 +1,13 @@
 #include "../index.h"
 
+static void
+define_builtin(mod_t *mod, const char *name, builtin_t *builtin) {
+    mod_define(mod, (spec_t *) builtin_spec_new(name, builtin));
+}
+
 void
-import_builtins(mod_t *self) {
-    mod_define(self, (spec_t *) builtin_spec_new("@wire/connect", builtin_wire_connect));
-    mod_define(self, (spec_t *) builtin_spec_new("@wire/print-net", builtin_wire_print_net));
-    mod_define(self, (spec_t *) builtin_spec_new("@worker/interact", builtin_worker_interact));
+import_builtins(mod_t *mod) {
+    define_builtin(mod, "@wire/connect", builtin_wire_connect);
+    define_builtin(mod,  "@wire/print-net", builtin_wire_print_net);
+    define_builtin(mod, "@worker/interact", builtin_worker_interact);
 }
