d7e5ebef6159b430f4205ed25a4885679549a732
Xie Yuheng <xyheme@gmail.com>
2024-11-20 12:12:29 +0800

    up

--- scripts/run-examples.sh
+++ scripts/run-examples.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 for example in $(find examples -name '*.test.inet'); do
-    echo "[snapshot test] $example"
+    echo "[snapshot-test] $example"
     ./bin/inet run $example
 done
 
 for example in $(find examples -name '*.error.inet'); do
-    echo "[snapshot error] $example"
+    echo "[snapshot-error] $example"
     ./bin/inet run $example || true
 done
