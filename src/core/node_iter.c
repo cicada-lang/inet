08f12238d003f0c183553888a035ffc5e9d561cd
Xie Yuheng <xyheme@gmail.com>
2024-12-11 05:18:18 +0800

    [core] `node_iter_new`

--- src/core/node_iter.c
+++ src/core/node_iter.c
@@ -5,3 +5,12 @@ struct node_iter_t {
     list_t *occurred_node_list;
     list_t *remaining_node_list;
 };
+
+node_iter_t *
+node_iter_new(node_t *root) {
+    node_iter_t *self = new(node_iter_t);
+    self->root = root;
+    self->occurred_node_list = list_new();
+    self->remaining_node_list = list_new();
+    return self;
+}
--- src/core/node_iter.h
+++ src/core/node_iter.h
@@ -1,1 +1,3 @@
 #pragma once
+
+node_iter_t *node_iter_new(node_t *root);
