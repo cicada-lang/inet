3bfa5a79669cb8046ae569069313d7ab96669812
Xie Yuheng <xyheme@gmail.com>
2024-12-12 03:13:31 +0800

    [canvas] `canvas_draw_line_low` & `canvas_draw_line_high`

--- src/modules/canvas/canvas_draw_line.c
+++ src/modules/canvas/canvas_draw_line.c
@@ -9,12 +9,26 @@ canvas_draw_line_low(
     size_t x1, size_t y1,
     uint32_t pixel
 ) {
-    (void) self;
-    (void) x0;
-    (void) y0;
-    (void) x1;
-    (void) y1;
-    (void) pixel;
+    int32_t dx = x1 - x0;
+    int32_t dy = y1 - y0;
+    int32_t yi = 1;
+    if (dy < 0) {
+        yi = -1;
+        dy = -dy;
+    }
+
+    int32_t D = (2 * dy) - dx;
+    int32_t y = y0;
+
+    for (size_t x = x0; x <= x1; x++) {
+        canvas_draw_pixel(self, x, y, pixel);
+        if (D > 0) {
+            y = y + yi;
+            D = D + (2 * (dy - dx));
+        } else {
+            D = D + 2*dy;
+        }
+    }
 }
 
 static void
@@ -24,12 +38,26 @@ canvas_draw_line_high(
     size_t x1, size_t y1,
     uint32_t pixel
 ) {
-    (void) self;
-    (void) x0;
-    (void) y0;
-    (void) x1;
-    (void) y1;
-    (void) pixel;
+    int32_t dx = x1 - x0;
+    int32_t dy = y1 - y0;
+    int32_t xi = 1;
+    if (dx < 0) {
+        xi = -1;
+        dx = -dx;
+    }
+
+    int32_t D = (2 * dx) - dy;
+    int32_t x = x0;
+
+    for (size_t y = y0; y <= y1; y++) {
+        canvas_draw_pixel(self, x, y, pixel);
+        if (D > 0) {
+            x = x + xi;
+            D = D + (2 * (dx - dy));
+        } else {
+            D = D + 2*dx;
+        }
+    }
 }
 
 void
