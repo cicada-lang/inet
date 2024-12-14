00027d641bd0001acbff9f44a61a8c5a63d0aebd
Xie Yuheng <xyheme@gmail.com>
2024-12-14 11:06:02 +0800

    up

--- todo/nat.lisp
+++ todo/nat.lisp
@@ -2,15 +2,13 @@
 (node add1 prev value!)
 (node add target! addend result)
 
-(rule (zero value!)
-      (add target! addend result)
+(rule (add (zero) addend result)
   (connect addend result))
 
-(rule (add1 prev value!)
-      (add target! addend result)
+(rule (add (add1 prev) addend result)
   (add1 (add prev addend) result))
 
-;; (- test -)
+;; test
 
 (wire-print-net
  (interact
