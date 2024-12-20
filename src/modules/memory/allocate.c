667836248ef1e3c25e68d541b4313d1e815fc4a7
Xie Yuheng <xyheme@gmail.com>
2024-12-21 03:53:11 +0800

    up

--- src/modules/memory/allocate.c
+++ src/modules/memory/allocate.c
@@ -1,24 +1,28 @@
 #include "index.h"
 
-static bool
-is_16_byte_aligned(void *address) {
-    return (((uintptr_t) address) & ((uintptr_t) 0xf)) == 0;
+// The address of a block returned by malloc or realloc in GNU systems
+// is always a multiple of eight (or sixteen on 64-bit systems).
+// -- https://www.gnu.org/software/libc/manual/html_node/Aligned-Memory-Blocks.html
+
+bool
+pointer_is_8_bytes_aligned(void *pointer) {
+    return (((uintptr_t) pointer) & ((uintptr_t) 0x7)) == 0;
 }
 
 void *
 allocate(size_t size) {
-    void *address = calloc(1, size);
-    assert(address);
-    assert(is_16_byte_aligned(address));
-    return address;
+    void *pointer = calloc(1, size);
+    assert(pointer);
+    assert(pointer_is_8_bytes_aligned(pointer));
+    return pointer;
 }
 
 void *
 allocate_many(size_t size, size_t unit_size) {
-    void *address = calloc(size, unit_size);
-    assert(address);
-    assert(is_16_byte_aligned(address));
-    return address;
+    void *pointer = calloc(size, unit_size);
+    assert(pointer);
+    assert(pointer_is_8_bytes_aligned(pointer));
+    return pointer;
 }
 
 void *
--- src/modules/memory/allocate.h
+++ src/modules/memory/allocate.h
@@ -1,5 +1,7 @@
 #pragma once
 
+bool pointer_is_8_bytes_aligned(void *pointer);
+
 void *allocate(size_t size);
 void *allocate_many(size_t size, size_t unit_size);
 void *allocate_pointers(size_t size);
