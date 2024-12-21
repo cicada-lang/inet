e09cddcbc38bc490e324b438b236fb70266c96c2
Xie Yuheng <xyheme@gmail.com>
2024-12-21 20:42:36 +0800

    up

--- run-examples.sh
+++ run-examples.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-app=inet
+app=./bin/inet
 ext=inet
 
 for example in $(find examples -name "*.${ext}" -not -name "*.test.${ext}" -not -name "*.error.${ext}" -not -name "*.play.${ext}"); do
     echo "[run] $example"
-    ./bin/${app} run $example
+    ${app} run $example
 done
 
 for example in $(find examples -name "*.test.${ext}"); do
     echo "[test] $example"
-    ./bin/${app} run $example > $example.out
+    ${app} run $example > $example.out
 done
 
 for example in $(find examples -name "*.error.${ext}"); do
     echo "[error] $example"
-    ./bin/${app} run $example 2> $example.err || true
+    ${app} run $example 2> $example.err || true
 done
