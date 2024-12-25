ee12c42972696bf8c405769d430a8a745979344d
Xie Yuheng <xyheme@gmail.com>
2024-12-25 11:35:59 +0800

    [sexp] symbol_sexp_t has token

--- src/modules/sexp/sexp.h
+++ src/modules/sexp/sexp.h
@@ -1,5 +1,5 @@
 #pragma once
 
 struct sexp_t { sexp_kind_t kind; };
-struct symbol_sexp_t { sexp_kind_t kind; char *string; };
+struct symbol_sexp_t { sexp_kind_t kind; token_t *token; };
 struct list_sexp_t { sexp_kind_t kind; list_t *sexp_list; };
