aaad0cb29d40aac1e0badbca245c64ab28fe5ae9
Xie Yuheng <xyheme@gmail.com>
2024-12-30 16:39:12 +0800

    [primitive] import_all -- fix define_constant

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 remove def_as_primitive_def def_as_function_def def_as_node_def
 
-[primitive] import_all -- fix define_constant
 [primitive] import_all -- int, float -- [maybe] use full prefix
 
 [lang] execute_int
--- src/primitives/import_all.c
+++ src/primitives/import_all.c
@@ -4,12 +4,12 @@ void
 import_all(mod_t *mod) {
     // null
 
-    // define_constant(mod, "null", xnull);
+    define_constant(mod, "null", xnull);
 
     // bool
 
-    // define_constant(mod, "false", xfalse);
-    // define_constant(mod, "true", xtrue);
+    define_constant(mod, "false", xfalse);
+    define_constant(mod, "true", xtrue);
     define_primitive_1_fn(mod, "not", xbool_not);
     define_primitive_2_fn(mod, "and", xbool_and);
     define_primitive_2_fn(mod, "or", xbool_or);
