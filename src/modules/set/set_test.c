1bb81dc674276ae19f118f20a09bedede6c14c84
Xie Yuheng <xyheme@gmail.com>
2024-12-31 15:34:16 +0800

    up

--- src/modules/set/set_test.c
+++ src/modules/set/set_test.c
@@ -8,17 +8,36 @@ set_test(void) {
         set_t *set = set_new();
         assert(set_length(set) == 0);
 
-        set_add(set, (void *) 1);
-        set_add(set, (void *) 2);
-        set_add(set, (void *) 3);
+        assert(set_add(set, (void *) 1));
+        assert(set_add(set, (void *) 2));
+        assert(set_add(set, (void *) 3));
 
         assert(set_length(set) == 3);
 
-        set_add(set, (void *) 1);
-        set_add(set, (void *) 2);
-        set_add(set, (void *) 3);
+        assert(!set_add(set, (void *) 1));
+        assert(!set_add(set, (void *) 2));
+        assert(!set_add(set, (void *) 3));
 
         assert(set_length(set) == 3);
+
+        assert(set_has(set, (void *) 1));
+        assert(set_has(set, (void *) 2));
+        assert(set_has(set, (void *) 3));
+
+        assert(!set_has(set, (void *) 0));
+        assert(!set_has(set, (void *) 4));
+
+        assert(set_delete(set, (void *) 2));
+        assert(set_delete(set, (void *) 3));
+
+        assert(!set_delete(set, (void *) 2));
+        assert(!set_delete(set, (void *) 3));
+
+        assert(set_length(set) == 1);
+
+        assert(set_has(set, (void *) 1));
+        assert(!set_has(set, (void *) 2));
+        assert(!set_has(set, (void *) 3));
 
         set_destroy(&set);
     }
