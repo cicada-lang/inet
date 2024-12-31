cbefad10c544f3067f4e84f25c3e840a0ef09550
Xie Yuheng <xyheme@gmail.com>
2024-12-31 15:30:52 +0800

    up

--- src/modules/set/set_test.c
+++ src/modules/set/set_test.c
@@ -4,12 +4,24 @@ void
 set_test(void) {
     printf("<set_test>\n");
 
-    set_t *set = set_new();
-    assert(set_length(set) == 0);
+    {
+        set_t *set = set_new();
+        assert(set_length(set) == 0);
 
-    //
+        set_add(set, (void *) 1);
+        set_add(set, (void *) 2);
+        set_add(set, (void *) 3);
 
-    set_destroy(&set);
+        assert(set_length(set) == 3);
+
+        set_add(set, (void *) 1);
+        set_add(set, (void *) 2);
+        set_add(set, (void *) 3);
+
+        assert(set_length(set) == 3);
+
+        set_destroy(&set);
+    }
 
     printf("</set_test>\n");
 }
